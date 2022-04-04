f40b89e810ce370a7c43e8bcdad9063e2778af2c
major1balaz525 <major.balazs@telekom.hu>
2022-04-04 23:20:42 +0200

    fix(#CSAK-0000): throw exception

--- src/main/kotlin/com/example/resilence/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/resilence/demo/DemoApplication.kt
@@ -5,35 +5,45 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import reactor.core.publisher.Flux
-import java.lang.RuntimeException
 import java.time.Duration
-import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Consumer
 import java.util.function.Function
 import java.util.function.Supplier
+import kotlin.random.Random
 
 @SpringBootApplication
 class ReactiveProcessorApplication {
+
 	private val logger = LogFactory.getLog(javaClass)
-	private val atomicInteger = AtomicInteger()
+
+	private val atomicInteger = AtomicInteger(1)
+
 	@Bean
 	fun aggregate(): Function<Flux<String>, Flux<String>> {
 		return Function { inbound: Flux<String> ->
-			inbound.log()
-				.window(Duration.ofSeconds(30), Duration.ofSeconds(5))
+			val pipeline = inbound.log()
+				.window(Duration.ofSeconds(5), Duration.ofSeconds(5))
 				.flatMap { w: Flux<String> ->
 					w.reduce(
-						" "
-					) { s1: String, s2: String -> "$s1 $s2" }
+						""
+					) { s1: String, s2: String ->
+						val index = if (s2.toInt() % 10 == 0) -1 else 0
+						s2[index]
+						"$s1 $s2"
+					}
 				}
 				.log()
+			addOnErrorResume(pipeline)
 		}
 	}
 
-	//Following source and sinks are used for testing only.
-	//Test source will send data to the same destination where the processor receives data
-	//Test sink will consume data from the same destination where the processor produces data
+	private fun <T> addOnErrorResume(pipeline: Flux<T>): Flux<T> =
+		pipeline.onErrorResume {
+			addOnErrorResume(pipeline)
+		}
+
+
 	@Bean
 	fun testSource(): Supplier<String> {
 		return Supplier { atomicInteger.getAndAdd(1).toString() }
